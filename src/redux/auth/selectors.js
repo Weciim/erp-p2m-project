d91cdc279928b63d17f42135458dfc62aa90e0be
Wecim Derwich <wassimderwich0@gmail.com>
2025-02-07 08:56:10 +0100

    Revert "first commit"
    This reverts commit b64065b1541ed59790310b66a666c829c42d44e8.

--- src/redux/auth/selectors.js
+++
@@ -1,6 +0,0 @@
-import { createSelector } from 'reselect';
-
-export const selectAuth = (state) => state.auth;
-export const selectCurrentAdmin = createSelector([selectAuth], (auth) => auth.current);
-
-export const isLoggedIn = createSelector([selectAuth], (auth) => auth.isLoggedIn);
--- src/request/request.js
+++
@@ -1,2 +0,0 @@
-const request = {};
-export default { request };
