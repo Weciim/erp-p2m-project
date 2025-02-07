b64065b1541ed59790310b66a666c829c42d44e8
Wecim Derwich <wassimderwich0@gmail.com>
2025-02-07 08:48:39 +0100

    first commit

---
+++ src/redux/rootReducer.js
@@ -0,0 +1,12 @@
+import { combineReducers } from 'redux';
+
+import { reducer as authReducer } from './auth';
+
+
+// Combine all reducers.
+
+const rootReducer = combineReducers({
+  auth: authReducer
+});
+
+export default rootReducer;
